426fc675a1f8000a54581b0a075c39c0a685d7f7
Joselo <jose.manzano@yachaytech.edu.ec>
2022-07-18 19:18:58 -0500

    calculando_el_area_y_el_canvas

--- Programa/Producto.cs
+++ Programa/Producto.cs
@@ -18,10 +18,10 @@ namespace Programa
         static void Main(string[] args)
         {
             Canvas c1 = new Canvas();
-            c1.alturaCanvas = 50;
-            c1.anchoCanvas = 150;
+            c1.alturaCanvas = 10;
+            c1.anchoCanvas = 50;
             Console.WriteLine("area del canvas: " + c1.canvasArea());
-
+            double area = c1.canvasArea();
 
             string file = @"C:\json\figuras.json";
             string Jsontext = File.ReadAllText(file);
@@ -51,6 +51,7 @@ namespace Programa
             re1.baseR = 5;
             re1.altR = 2;
             re1.AreaRectangulo();
+            double areaR = re1.AreaRectangulo();
 
 
             Triangulo tr1 = new Triangulo();
@@ -60,14 +61,51 @@ namespace Programa
             tr1.arista1 = 10;
             tr1.arista1 = 10;
             tr1.areaTriangulo();
-
+            double areaT = tr1.areaTriangulo();
 
             Circulo ci1 = new Circulo();
             ci1.Nombre = "Circulo";
             ci1.Texto = "Esferico";
             ci1.radio = 4;
             ci1.areaCirculo();
+            double areaC = ci1.areaCirculo();
 
+            double areaResta = area;
+
+           Console.WriteLine();
+           if(area > areaR)
+            {
+                Console.WriteLine("-Se dibujo el rectangulo");
+                areaResta = area - areaR;
+                Console.WriteLine("Quedan: " + Math.Round(areaResta, 2) + "cm");
+
+            }
+            else
+            {
+                Console.WriteLine("no se puede dibujar, no hay espacio");
+            }
+
+            if (areaResta > areaT)
+            {
+                Console.WriteLine("-Se dibujo el triangulo");
+                areaResta = area - areaT;
+                Console.WriteLine("Quedan: " + Math.Round(areaResta, 2) + "cm");
+            }
+            else
+            {
+                Console.WriteLine("no se puede dibujar, no hay espacio");
+            }
+
+            if (areaResta > areaC)
+            {
+                Console.WriteLine("-Se dibujo el circulo");
+                areaResta = area - areaC;
+                Console.WriteLine("Quedan: " + Math.Round(areaResta, 2) + "cm");
+            }
+            else
+            {
+                Console.WriteLine("no se puede dibujar, no hay espacio");
+            }
 
         }
 
